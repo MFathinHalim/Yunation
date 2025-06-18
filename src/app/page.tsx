09ca57502b595378818c5536f8f43218f0658199
M.Fathin Halim <halimfathin7@gmail.com>
2025-06-18 07:01:16 +0700

    make the first design on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen relative z-10">
+        <div className="flex justify-between items-center w-[70vw] z-10">
+          <h1 className="text-4xl font-bold">Yunation</h1>
+          <a className="bg-[#FFF0F4] border border-[#B7B1F2] text-center rounded-full mb-1 font-bold py-2 px-7">About Us</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="bg-white border shadow w-[70vw] max-h-[70vh] mx-8 my-1 z-10 flex flex-col lg:flex-row">
+          <div className="flex flex-col w-full lg:w-2/3">
+            <div className="flex h-1/2">
+              <img
+                className="w-1/2 h-full object-cover border"
+                src="https://pbs.twimg.com/media/F9b-dY9aMAAac0P.jpg"
+                alt="img1"
+              />
+              <img
+                className="w-1/2 h-full object-cover border"
+                src="https://pbs.twimg.com/media/F2vzXNlaMAAO6Gi.jpg"
+                alt="img2"
+              />
+            </div>
+
+            <iframe
+              className="w-full h-50 lg:h-100 border"
+              src="https://www.youtube.com/embed/M89C6vstdbw?si=Ujb2Q5qu8aboFJk7"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+
+          <div className="hidden lg:block w-full lg:w-1/2 h-auto">
+            <img
+              className="w-full h-full object-cover border"
+              src="https://pbs.twimg.com/media/GDMouUxaUAAUuSG.jpg:large"
+              alt="big img"
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-6 mt-3 z-10">
+          <a className="bg-[#FFE8CD] hover:bg-[#FFE8CD]/70 px-7 py-2 border shadow">Instagram</a>
+          <a className="bg-[#FFF2EB] hover:bg-[#FFF2EB]/70 px-7 py-2 border shadow">Discord</a>
+          <a className="bg-[#FFDCDC] hover:bg-[#FFDEEE]/70 px-7 py-2 border shadow">Youtube</a>
+        </div>
+
+        <div className="absolute bottom-0 left-0 w-full z-0 pointer-events-none">
+          <img src="/wave.svg" className="w-full" />
+        </div>
+      </div>
+
+      <div className="fixed bottom-4 right-4 z-20">
+        <img src="/chibiyuna.png" className="h-30" />
+      </div>
+    </>
   );
 }
